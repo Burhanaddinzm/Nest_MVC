9fb459089bbb2bbfc70a6c02b1709412ad72007a
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-03-06 22:42:47 +0400

    Nest MVC First Commit

---
+++ Nest/Models/ProductImage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nest.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } = null!;
+        [NotMapped]
+        public IFormFile File { get; set; } = null!;
+        public bool IsMain { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+    }
+}
---
+++ Nest/Models/Vendor.cs
@@ -0,0 +1,13 @@
+﻿namespace Nest.Models
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Adress { get; set; } = null!;
+        public string ContactInfo { get; set; } = null!;
+        public double? Rating { get; set; }
+        public List<Product>? Products { get; set; }
+    }
+}
